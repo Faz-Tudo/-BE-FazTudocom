9554182fe9b599912cb59b8f2528c6c0aa99747b
WallaceMartinz <wallace.mota@sptech.school>
2024-06-18 20:58:35 -0300

    teste CI/CD

--- faztudo.comLOCAL/src/main/java/sptech/faztudo/comLOCAL/Application.java
+++ faztudo.comLOCAL/src/main/java/sptech/faztudo/comLOCAL/Application.java
@@ -12,8 +12,7 @@ import java.sql.Connection;
 
 @SpringBootApplication
 public class  Application {
-
-	//Inicio da aplicação
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
